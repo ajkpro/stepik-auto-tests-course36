a118d39d290c8304bad4142ce750c4cb74fdad99
ajkpro <94987435+ajkpro@users.noreply.github.com>
2021-12-28 07:53:19 +0300

    Create test_items.py

---
+++ test_items.py
@@ -0,0 +1,22 @@
+"""
+Ссылка;
+http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/
+
+Запуск:
+pytest --language=es test_items.py
+"""
+import pytest
+from selenium import webdriver
+import time
+
+@pytest.mark.parametrize('language', ["ru"])    #, "es", "en"
+def test_find_card_button(browser, language):
+    try: 
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        browser.get(link)
+        time.sleep(3)
+        browser.find_element_by_css_selector("#login_link")
+        button=browser.find_element_by_class_name('btn-add-to-basket')
+        assert button is not None, 'asserError'
+    finally:
+        browser.close()
